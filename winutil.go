699c7196573298afc2aa244d3c74685d2f793cfe
Charlie Vieth <charlie.vieth@gmail.com>
2017-04-16 16:38:57 -0400

    Remove unused Process Tree code

--- winutil.go
+++ winutil.go
@@ -9,7 +9,6 @@ import (
 	"os"
 	"path/filepath"
 	"runtime"
-	"sort"
 	"strconv"
 	"syscall"
 	"time"
@@ -54,109 +53,6 @@ func EnumProcesses() ([]uint32, error) {
 	}
 	n := read / uint32(unsafe.Sizeof(pids[0]))
 	return pids[:n], nil
-}
-
-type uint32Slice []uint32
-
-func (p uint32Slice) Len() int           { return len(p) }
-func (p uint32Slice) Less(i, j int) bool { return p[i] < p[j] }
-func (p uint32Slice) Swap(i, j int)      { p[i], p[j] = p[j], p[i] }
-
-type Process struct {
-	Pid      uint32
-	Parent   *Process
-	Children map[uint32]*Process
-}
-
-func sortPids(p []uint32) {
-	n := len(p)
-	for i := n - 1; i > 0; i-- {
-		if p[i] < p[i-1] {
-			fmt.Println("sortPids: not sorted")
-			sort.Sort(uint32Slice(p))
-			return
-		}
-	}
-	fmt.Println("sortPids: sorted")
-}
-
-type expProcess struct {
-	Pid      uint32
-	Parent   uint32
-	Children []expProcess
-}
-
-func (p *Process) ToJSON() expProcess {
-	x := expProcess{Pid: p.Pid}
-	if p.Parent != nil {
-		x.Parent = p.Parent.Pid
-	}
-	if len(p.Children) != 0 {
-		for _, c := range p.Children {
-			x.Children = append(x.Children, c.ToJSON())
-		}
-	}
-	return x
-}
-
-func CreateTree() (*Process, error) {
-	pids, err := EnumProcesses()
-	if err != nil && len(pids) == 0 {
-		return nil, err
-	}
-	sortPids(pids)
-
-	// sanity check
-	if pids[0] != 0 {
-		panic("CreateTree: missing PID 0 from process list")
-	}
-	if len(pids) == 1 {
-		panic("CreateTree: failed to enumerate process")
-	}
-	if pids[1] == 0 {
-		panic("CreateTree: duplicate PID 0 entry in process list")
-	}
-
-	// manually add PID 0
-	procs := make(map[uint32]*Process, len(pids))
-	head := &Process{
-		Pid:    0,
-		Parent: nil,
-	}
-	procs[head.Pid] = head
-
-	for _, pid := range pids[1:] {
-		ppid, err := ParentPID(pid)
-		if err != nil {
-			fmt.Printf("CreateTree (%d): %s\n", pid, err)
-			continue
-		}
-		pp, ok := procs[ppid]
-		if !ok {
-			pp = &Process{
-				Pid:      ppid,
-				Children: make(map[uint32]*Process),
-			}
-			procs[ppid] = pp
-		}
-		if pp.Children == nil {
-			pp.Children = make(map[uint32]*Process)
-		}
-		cp, ok := procs[pid]
-		if !ok {
-			cp = &Process{
-				Pid:    pid,
-				Parent: pp,
-			}
-			procs[pid] = cp
-		}
-		if cp.Parent == nil {
-			cp.Parent = pp
-		}
-		pp.Children[pid] = cp
-	}
-
-	return head, nil
 }
 
 // _PROCESS_BASIC_INFORMATION
@@ -330,6 +226,8 @@ func main() {
 	fmt.Println("error:", err)
 }
 
+// WARN: Dev only helper functions
+
 func PrintJSON(v interface{}) error {
 	b, err := json.MarshalIndent(v, "", "    ")
 	if err == nil {
@@ -362,34 +260,3 @@ func Fatal(err interface{}) {
 	}
 	os.Exit(1)
 }
-
-// func main() {
-// 	if len(os.Args) != 2 {
-// 		fmt.Fprintln(os.Stderr, "Invalid args...")
-// 	}
-// 	parent, err := strconv.Atoi(os.Args[1])
-// 	if err != nil {
-// 		fmt.Fprintln(os.Stderr, err)
-// 	}
-// 	if err := RecursiveKill(parent); err != nil {
-// 		fmt.Fprintln(os.Stderr, err)
-// 	}
-// }
-
-/*
-// appendChildren recursively appends the child pids of parent pid to slice a.
-func appendChildren(a []uint32, parent uint32, m map[uint32][]uint32) []uint32 {
-	if pids, ok := m[parent]; ok && len(pids) != 0 {
-		// Add children first
-		a = append(a, pids...)
-		// Add grandchildren, if any...
-		for _, pid := range pids {
-			// WARN (CEV): not tested - attempt to prevent infinite loop
-			if pid != parent {
-				a = appendChildren(a, pid, m)
-			}
-		}
-	}
-	return a
-}
-*/
